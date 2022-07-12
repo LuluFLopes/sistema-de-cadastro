bd8be2aa50bbba3ad8a632d2ca8f53364488a6c5
Luciano <luc.merce@hotmail.com>
2022-07-12 16:03:24 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando o sistema de gerenciamento e jogos");
 console.log("Aplicação está se conectando com o banco de dados");
 // mais um console log de teste:
 console.log("O paulo fez esse commit sem voce ver na aula!");
-console.log("Enviando para o GitHub");
+
